9b7a55b6080984f2454ed0cb5842eb42fc98e10b
tgcorias <ticianocorias@gmail.com>
2022-05-08 19:53:58 -0300

    [ADD] deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default {
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
   base: 'https://tgcorias.github.io/rosaliapp/'
-}
+})
